27999f72d87746be1405e2ee0a4bc9738fe0fe7f
Biswajit Mandal <bmandal@juniper.net>
2023-06-14 07:08:22 -0700

    Bind the docker network on host

--- scale_snmp_servers.sh
+++ scale_snmp_servers.sh
@@ -3,5 +3,5 @@
 docker build -t snmp-simulator .
 for port in {9001..9016}
 do
-    docker run --name snmp_simulator_$port -d -e SNMP_PORT=$port -p $port:$port snmp-simulator:latest
+    docker run --name snmp_simulator_$port -d --network host -e SNMP_PORT=$port snmp-simulator:latest
 done
